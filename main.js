a948a5c0374e00627acc6d8e144046c7ecbe6429
unknown <pablicjs@gmail.com>
2023-02-13 17:38:03 -0300

    completing basic functions

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, shell } = require('electron');
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform = 'darwin';
 
-
+//* Create the main window
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
         width: isDev ? 1000 : 500,
-        height: 600
+        height: 600,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     //* Open devtools if in dev env
@@ -20,8 +25,26 @@ function createMainWindow(){
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+//* Create about window
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Image Resizer',
+        width: 300,
+        height: 300
+    });
+
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+//* App is ready
 app.whenReady().then(() => {
+    console.log(process.platform)
     createMainWindow();
+
+    //* Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu)
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -29,6 +52,50 @@ app.whenReady().then(() => {
         }
     })
 });
+
+const menu = [
+    {
+        role: 'fileMenu'
+    },
+    ...(isMac ? [{
+        label: app.name,
+        submenu:[
+            {
+                label: 'About',
+                click: createAboutWindow
+            },
+            {
+                label: 'Follow me',
+                click: () => shell.openExternal('https://github.com/pablixs')
+            },
+        ],
+    }] : []),
+    ...(!isMac ? [{
+        label: 'Help',
+        submenu: [
+            {
+                label: 'About',
+                click: createAboutWindow
+            },
+            {
+            label: 'About',
+            click: () => shell.openExternal('https://github.com/pablixs')
+            }]}] : []),
+    // {
+    //     label: 'File',
+    //     submenu: [
+    //         {
+    //             label: 'Quit',
+    //             click: () => app.quit(),
+    //             accelerator: 'CmdOrCtrl + W',
+    //         },
+    //         {
+    //             label: 'Follow us',
+    //             click: () => shell.openExternal('https://github.com/pablixs')
+    //         }
+    //     ]
+    // }
+];
 
 app.on('window-all-closed', () => {
     if (!isMac){
---
+++ renderer/js/renderer.js
@@ -0,0 +1,80 @@
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage(e) {
+    const file = e.target.files[0];
+
+    if(!isFileImage(file)){
+        alertError('Please select an image');
+        return
+    }
+
+    //* Get original dimension
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = function () {
+        widthInput.value = this.width;
+        heightInput.value = this.height;
+    }
+
+    form.style.display = 'block';
+    filename.innerHTML = file.name;
+    outputPath.innerText = path.join(os.homedir(), 'imageresizer')
+}
+
+//* Send image data to main
+function sendImage(e){
+    e.preventDefault();
+
+    const width = widthInput.value
+    const height = heightInput.value
+
+    if(!img.files[0]){
+        alertError('Please upload an image');
+        return
+    }
+
+    if(width === '' || height === ''){
+        alertError('Please fill in a height and width');
+        return
+    }
+}
+
+//* Make sure file is image
+function isFileImage(file) {
+    const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg', 'image/jpg']
+    return file && acceptedImageTypes.includes(file['type'])
+}
+
+function alertError(message){
+    Toastify.toast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+            background: 'red',
+            color: 'white',
+            textAlign: 'center'
+        }
+    })
+}
+
+function alertSuccess(message){
+    Toastify.toast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+            background: 'green',
+            color: 'white',
+            textAlign: 'center'
+        }
+    })
+}
+
+img.addEventListener('change', loadImage);
+form.addEventListener('submit', sendImage)
